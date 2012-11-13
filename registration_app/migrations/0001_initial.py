3963017571d31481d074758f79b481b3ecf667d3
Oskar Ehnstrom <oskar@ehnstrom.net>
2012-11-13 11:36:09 +0200

    Modified models

--- registration_app/migrations/0001_initial.py
+++ registration_app/migrations/0001_initial.py
@@ -8,13 +8,67 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Participant'
+        db.create_table('registration_app_participant', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=90)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('phone', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('allergies', self.gf('django.db.models.fields.TextField')()),
+            ('type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['registration_app.ParticipantType'])),
+            ('avec', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['registration_app.Participant'], null=True, blank=True)),
+        ))
+        db.send_create_signal('registration_app', ['Participant'])
+
+        # Adding model 'Event'
+        db.create_table('registration_app_event', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date', self.gf('django.db.models.fields.TimeField')()),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=120)),
+        ))
+        db.send_create_signal('registration_app', ['Event'])
+
+        # Adding model 'ParticipantType'
+        db.create_table('registration_app_participanttype', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+        ))
+        db.send_create_signal('registration_app', ['ParticipantType'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'Participant'
+        db.delete_table('registration_app_participant')
+
+        # Deleting model 'Event'
+        db.delete_table('registration_app_event')
+
+        # Deleting model 'ParticipantType'
+        db.delete_table('registration_app_participanttype')
+
 
     models = {
-        
+        'registration_app.event': {
+            'Meta': {'object_name': 'Event'},
+            'date': ('django.db.models.fields.TimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120'})
+        },
+        'registration_app.participant': {
+            'Meta': {'object_name': 'Participant'},
+            'allergies': ('django.db.models.fields.TextField', [], {}),
+            'avec': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['registration_app.Participant']", 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '90'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['registration_app.ParticipantType']"})
+        },
+        'registration_app.participanttype': {
+            'Meta': {'object_name': 'ParticipantType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
     }
 
-    complete_apps = ['tf141']
+    complete_apps = ['registration_app']
--- registration_app/models.py
+++ registration_app/models.py
@@ -1,3 +1,4 @@
+# --coding: UTF-8 --
 from django.db import models
 
 
@@ -12,19 +13,35 @@ class Participant(models.Model):
     phone = models.CharField(max_length=20, verbose_name="Telefonnummer")
     
     # Allergies
-    allergies = models.TextField(verbose_name="Allergier")
+    allergies = models.TextField(verbose_name="Allergier", blank=True)
     
     # Student or not
     type = models.ForeignKey('ParticipantType', verbose_name="Deltagartyp")
     
     # Avec
-    avec = models.ForeignKey('Participant', verbose_name="Avec", null=True, blank=True)
+    avec = models.OneToOneField('Participant', verbose_name="Avec", null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name
     
 class Event(models.Model):
     # Date&Time
     date = models.TimeField()
     
+    # Name
+    name = models.CharField(max_length=120, verbose_name="Händelsens namn")
+    
+    def __unicode__(self):
+        return self.name
     
 class ParticipantType(models.Model):
+    
     # Name of type
     name = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return self.name
+    
+class Invoice(models.Model):
+    reference_number = models.PositiveIntegerField()
+    sum = models.FloatField()
