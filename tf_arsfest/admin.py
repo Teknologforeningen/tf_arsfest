06707aba55a9142f4c933f5ac286bf2b4312d924
Oskar Ehnstrom <oskar@ehnstrom.net>
2013-01-29 15:11:29 +0200

    Added export for registrations

--- tf_arsfest/admin.py
+++ tf_arsfest/admin.py
@@ -1,14 +1,13 @@
-from django.contrib import admin
-from models import Guest, Event, GuestType, Registration
+# --coding: UTF-8 --
 import csv
+from django.contrib import admin
 from django.http import HttpResponse
+from models import Guest, Event, GuestType, Registration
 
-admin.site.register(Registration)
-admin.site.register(GuestType)
-
+admin.site.register(GuestType) 
  
- 
-def export_as_csv(modeladmin, request, queryset):
+# Exportta alla gäster som hör till de valda eventsen
+def export_guests_as_csv(modeladmin, request, queryset):
     """ 
         Based on http://djangosnippets.org/snippets/2712/
     """
@@ -33,10 +32,63 @@ def export_as_csv(modeladmin, request, queryset):
             fields = [unicode(getattr(guest, field)).encode('utf-8') for field in field_names]
             fields.append(avec)
             writer.writerow(fields)
-    return response
+    return response    
+
+
+def prep_field(obj, field):
+    """ Returns the field as a unicode string. If the field is a callable, it
+    attempts to call it first, without arguments.
+    """
+    if '__' in field:
+        bits = field.split('__')
+        field = bits.pop()
+ 
+        for bit in bits:
+            obj = getattr(obj, bit, None)
+ 
+            if obj is None:
+                return ""
+ 
+    attr = getattr(obj, field)
+    output = attr() if callable(attr) else attr
+    return unicode(output).encode('utf-8') if output else ""
+ 
+ 
+def export_csv_action(description="Export as CSV", fields=None, exclude=None, header=True):
+    """ This function returns an export csv action. """
+    def export_as_csv(modeladmin, request, queryset):
+        """ Generic csv export admin action.
+        Based on http://djangosnippets.org/snippets/2712/
+        """
+        opts = modeladmin.model._meta
+        field_names = [field.name for field in opts.fields]
+        labels = []
+ 
+        if exclude:
+            field_names = [f for f in field_names if f not in exclude]
+ 
+        elif fields:
+            field_names = [field for field, _ in fields]
+            labels = [label for _, label in fields]
+ 
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=%s.csv' % (
+                unicode(opts).replace('.', '_')
+            )
+ 
+        writer = csv.writer(response)
+ 
+        if header:
+            writer.writerow(labels if labels else field_names)
+ 
+        for obj in queryset:
+            writer.writerow([prep_field(obj, field) for field in field_names])
+        return response
+    export_as_csv.short_description = description
+    return export_as_csv
 
 class EventModelAdmin(admin.ModelAdmin):
-    actions = [export_as_csv]
+    actions = [export_guests_as_csv]
  
 admin.site.register(Event, EventModelAdmin)
 
@@ -44,3 +96,25 @@ class GuestAdmin(admin.ModelAdmin):
     list_filter = ('event__name',)
     
 admin.site.register(Guest, GuestAdmin)
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_filter = ('event__name',)
+    
+    # Bestäm vad som exportas från registreringarna
+    actions = [
+        export_csv_action("Export Registrations as CSV",
+            fields=[
+                    ('event', 'Fest'),
+                    ('name', 'Namn'),
+                    ('solennakt', 'Solenn Akt'),
+                    ('greeting', 'Hälsning'),
+                    ('misc', 'Övrigt'),
+                    ('guest__name', 'Gäst'),
+                    ('avec__name', 'Avec'),
+            ],
+            header=True
+        ),
+               ]
+    
+admin.site.register(Registration, RegistrationAdmin)
+
