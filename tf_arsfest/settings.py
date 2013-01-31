20dd8fb7cc78b0fecf2589d11b4bf56ceb664ddc
Oskar Ehnstrom <oskar@ehnstrom.net>
2013-01-31 16:37:47 +0200

    Updated settings

--- tf_arsfest/settings.py
+++ tf_arsfest/settings.py
@@ -22,11 +22,6 @@ DATABASES = {
     }
 }
 
-
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
-
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 # Local time zone for this installation. Choices can be found here:
@@ -54,12 +49,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+#MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -76,7 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'media'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -130,9 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'tf_arsfest',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
 )
 
@@ -164,3 +157,9 @@ LOGGING = {
         },
     }
 }
+
+# Import local settings.
+try:
+    from local_settings import *
+except ImportError:
+    print "No local settings found."
