074693a724aa67f401414987212ff471f9f9df86
Oskar Ehnstrom <oskar@ehnstrom.net>
2013-10-21 16:52:45 +0300

    Updated wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,31 +1,15 @@
 """
-WSGI config for tf_arsfest project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+WSGI config
 """
-import os,sys
+import os, sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+path = '/var/www/arsfest/releases/current/'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tf_arsfest.settings")
+os.environ.setdefault("DJANGO_ENVIRONMENT", "hugin")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
