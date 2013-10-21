2c8c251500f7e859817e9422e5213f7642d1449b
Oskar Ehnstrom <oskar@ehnstrom.net>
2013-10-21 17:28:32 +0300

    Removed old reset script

--- reset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm tf_arsfest/sqlite.db 
-python manage.py  syncdb
-rm tf_arsfest/migrations/000*
-python manage.py schemamigration tf_arsfest --initial
-python manage.py migrate tf_arsfest
